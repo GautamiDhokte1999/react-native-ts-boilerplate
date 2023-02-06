98699d713f65fe1f67b043ce7134d88e2632471a
GautamiDhokte1999 <gautamid@winjit.com>
2023-02-06 19:01:27 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-native-boilerplate
+Updated boilerplate with latest version of react native
+
+npx react-native init MyApp --template https://github.com/GautamiDhokte1999/react-native-ts-boilerplate.git
---
+++ template/src/Navigators/Application.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigator from './Main';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {styles} from './style'
+
+const Stack = createNativeStackNavigator();
+
+const ApplicationNavigator = () => (
+  <SafeAreaView style={styles.safeAreaStyle}>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {/* <Stack.Screen name="Startup" component={StartupContainer} /> */}
+        <Stack.Screen
+          name="Main"
+          component={MainNavigator}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </SafeAreaView>
+);
+
+export default ApplicationNavigator;
